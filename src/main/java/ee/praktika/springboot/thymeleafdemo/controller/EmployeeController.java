3998948dc2d00e2f093f1ed2073ec77c73e6be6c
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-29 11:58:18 +0200

    Removing the in-memory employee code.

--- src/main/java/ee/praktika/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/ee/praktika/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,9 +1,6 @@
 package ee.praktika.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,34 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ee.praktika.springboot.thymeleafdemo.entity.Employee;
+import ee.praktika.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping( "/employees" )
 public class EmployeeController {
 
-    //load employee data
-    private List<Employee> theEmployees;
+    private EmployeeService employeeService;
 
-    @PostConstruct
-    private void loadData(){
-
-        //create employees
-        Employee employee1 = new Employee( 1, "Lebron", "James", "LBJ@LAL.com" );
-        Employee employee2 = new Employee( 1, "Anthony", "Davis", "TheBrow@LAL.com" );
-        Employee employee3 = new Employee( 1, "Kobe", "Bryant", "MambaMentality@LAL.com" );
-
-        //create the list
-        theEmployees = new ArrayList<>();
-
-        //add to the list
-        theEmployees.add( employee1 );
-        theEmployees.add( employee2 );
-        theEmployees.add( employee3 );
+    public EmployeeController( EmployeeService theEmployeeService ) {
+        employeeService = theEmployeeService;
     }
 
     //add mapping for /list
     @GetMapping( "/list" )
     public String listEmployees( Model theModel ){
+
+        //get employees from the DB
+        List<Employee> theEmployees = employeeService.findAll();
 
         //add to the spring model
         theModel.addAttribute( "employees", theEmployees );
