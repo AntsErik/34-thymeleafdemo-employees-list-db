7e4f0aeac17336ba6749ea66d0523c6f762a6c8e
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-29 11:48:33 +0200

    Create new entity package and copy over employee Entity from 23-spring-data-jpa-cruddemo and add new contsructor with ID.

--- src/main/java/ee/praktika/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/ee/praktika/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ee.praktika.springboot.thymeleafdemo.model.Employee;
+import ee.praktika.springboot.thymeleafdemo.entity.Employee;
 
 @Controller
 @RequestMapping( "/employees" )
